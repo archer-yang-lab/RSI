c32f70044d06646d600b4543e200299e0fc69f92
SpongeBob <weihua12138@outlook.com>
2026-03-10 22:27:11 -0400

    Add files via upload

---
+++ paper-settingII.py
@@ -0,0 +1,250 @@
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import pandas as pd
+import os
+import random
+from sklearn.model_selection import train_test_split
+import argparse
+import time
+import math
+import sys
+
+''' timer '''
+class Timer:
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end_time = time.time()
+        self.runtime = self.end_time - self.start_time
+#here we add a cost in eval_o function
+gray_cost=1
+red_green_cost=2
+def eval_new(Y, rejected_1, rejected_2, lower, higher):
+    # true positives for each threshold
+    true_reject_1 = np.sum(Y <= lower)
+    true_reject_2 = np.sum(Y >= higher)
+
+    # If rejected_1 is empty, all outputs set to 0 or np.nan as fallback
+    if len(rejected_1) == 0:
+        fdp_1 = 0.0
+        fdp_2 = 0.0
+        power1 = 0.0
+        power2 = 0.0
+    else:
+        # safe index
+        Y_r1 = Y[rejected_1]
+        if len(rejected_2) == 0:
+            fdp_1 = np.sum(lower < Y_r1) / len(rejected_1)
+            fdp_2 = np.sum((lower < Y_r1) & (higher > Y_r1)) / len(rejected_1)
+            power1 = np.sum(lower >= Y_r1) / true_reject_1 if true_reject_1 > 0 else 0.0
+            power2 = np.sum((lower >= Y_r1) | (higher <= Y_r1)) / (true_reject_1 + true_reject_2) if (true_reject_1 + true_reject_2) > 0 else 0.0
+        else:
+            union12 = np.array(list(set(rejected_1) | set(rejected_2)))
+            Y_union = Y[union12]
+            fdp_1 = np.sum(lower < Y[rejected_1]) / len(rejected_1)
+            fdp_2 = np.sum((lower < Y_union) & (higher > Y_union)) / len(union12)
+            power1 = np.sum(lower >= Y[rejected_1]) / true_reject_1 if true_reject_1 > 0 else 0.0
+            power2 = np.sum((lower >= Y_union) | (higher <= Y_union)) / (true_reject_1 + true_reject_2) if (true_reject_1 + true_reject_2) > 0 else 0.0
+
+    return fdp_1, fdp_2, power1, power2
+
+def BH(calib_scores, test_scores, q=0.1, extra_info=None):
+    ntest = len(test_scores)
+    ncalib = len(calib_scores)
+    pvals = np.zeros(ntest)
+
+    for j in range(ntest):
+        pvals[j] = (np.sum(calib_scores < test_scores[j]) + np.random.uniform(size=1)[0] * (
+                    np.sum(calib_scores == test_scores[j]) + 1)) / (ncalib + 1)
+
+    # BH(q)
+    df_test = pd.DataFrame({"id": range(ntest), "score": test_scores, "pval": pvals}).sort_values(by='pval')
+
+    df_test['threshold'] = q * np.linspace(1, ntest, num=ntest) / ntest
+    idx_smaller = [j for j in range(ntest) if df_test.iloc[j, 2] <= df_test.iloc[j, 3]]
+
+    if len(idx_smaller) == 0:
+        if not extra_info:
+            return (np.array([]))
+        elif extra_info == 'pval':
+            return np.array([]), pvals
+        else:
+            return np.array([]), df_test
+    else:
+        idx_sel = np.array(df_test.index[range(np.max(idx_smaller) + 1)])
+        if not extra_info:
+            return (idx_sel)
+        elif extra_info == 'pval':
+            return idx_sel, pvals
+        else:
+            return idx_sel, df_test
+
+parser = argparse.ArgumentParser()
+parser.add_argument('dataset', type=str)
+parser.add_argument('sample', type=float)
+parser.add_argument('seed', type=int)
+args = parser.parse_args()
+random.seed(args.seed)
+
+dataset_name = args.dataset
+dataset_path = os.path.join('data', f'{dataset_name}_training_disguised.csv')
+
+dataset = pd.read_csv(dataset_path)
+
+if args.sample < 1:
+    dataset = dataset.sample(frac=args.sample)
+
+thresholds_map1 = {'NK1': 8.5, 'PGP': 0.1, 'LOGD': 3, '3A4': 4.5, 'CB1': 6.5, 'DPP4': 6, 'HIVINT': 6, 'HIVPROT': 7, 'METAB': 40, 'OX1': 5.8, 'OX2': 6, 'PPB': 1, 'RAT_F': 1.0, 'TDI': 0, 'THROMBIN': 6}
+thresholds_map2 = {'NK1': 9.5, 'PGP': 0.8, 'LOGD': 5, '3A4': 6, 'CB1': 8.0, 'DPP4': 7, 'HIVINT': 7, 'HIVPROT': 9, 'METAB': 70, 'OX1': 7.5, 'OX2': 8, 'PPB': 2, 'RAT_F': 1.9, 'TDI': 1, 'THROMBIN': 9}
+
+
+threshold_1 = thresholds_map1[dataset_name]
+threshold_2 = thresholds_map2[dataset_name]
+
+total_Y = dataset['Act'].to_numpy()
+total_X = dataset.drop(columns=['MOLECULE', 'Act']).to_numpy()
+
+Xtc, Xtest, Ytc, Ytest = train_test_split(total_X, total_Y, test_size=15/100, shuffle=True) # tc: train and calib
+
+# ofdp_nominals = np.linspace(0.1, 0.5, 9)
+fdp_nominals = np.linspace(0.1, 1.0, 9)
+all_res = pd.DataFrame()
+epsilon = 1e-8
+
+# all_res['ofdp_nominals'] = ofdp_nominals
+all_res['fdp_nominals'] = fdp_nominals
+
+''' single stage'''
+def eval(Y, rejected, lower, higher):
+    r"""
+    Evaluate the selection correctness given the true values, the selected subsets, and the target region (lower,higher).
+
+    Args:
+        Y (np.ndarray): 1-d array of true responses (activity levels).
+        rejected (np.ndarray): 1-d array of the indices of rejected hypotheses.
+        lower, higher (float, float): The boundaries defining the desirable values of Y, which is (lower,higher).
+
+    Returns:
+        float, float, float: The empirical FDP, PCER and Power of the selection.
+    """
+    true_reject = np.sum((lower >= Y)|(Y >= higher))
+    if len(rejected) == 0:
+        fdp = 0
+        pcer = 0
+        power = 0
+    else:
+        fdp = np.sum((lower <= Y[rejected]) & (Y[rejected] < higher)) / len(rejected)
+        pcer = np.sum((lower <= Y[rejected]) & (Y[rejected] < higher)) / len(Y)
+        power = np.sum((lower > Y[rejected]) | (Y[rejected] >= higher)) / true_reject if true_reject != 0 else 0
+    return fdp, pcer, power
+
+''' single stage'''
+def eval_n(Y, rejected_1, lower, higher):
+    true_reject_1 = np.sum((lower >= Y)|(Y >= higher))
+    if len(rejected_1) == 0:
+        fdp = 0
+        power1 = 0
+        cost = 0
+        power = 0
+    else:
+        fdp = np.sum((lower < Y[rejected_1]) & (higher > Y[rejected_1])) / len(rejected_1)
+        power1 = np.sum((lower >= Y[rejected_1])|(higher <= Y[rejected_1])) / true_reject_1 if true_reject_1 != 0 else 0
+        set_1c = np.delete(Y, rejected_1)
+        cost = gray_cost*len(set_1c)
+        power = power1
+    return fdp, power1, cost, power
+
+def MCCS_score(y, pred, c_kL, c_kR, M):
+
+    is_inside = (y <= c_kL) | (y >= c_kR)
+    score_inside = M - np.minimum(c_kL -pred, pred-c_kR)
+    score_outside = -np.maximum( pred -c_kL, c_kR - pred)
+    scores = np.where(is_inside, score_inside, score_outside)
+
+    return scores
+
+''' single stage conformal selection '''
+fdpn_bcs, costn_bcs, powern_bcs, powers_bcs= [], [], [], []
+fdpn_cs, costn_cs, powern_cs, powers_cs= [], [], [], []
+from sklearn.ensemble import RandomForestClassifier
+with Timer() as timer:
+    rfc = RandomForestClassifier(n_estimators=100, max_depth=20, max_features='sqrt',criterion="entropy")
+    Xtrain, Xcalib, Ytrain, Ycalib = train_test_split(Xtc, Ytc, train_size=50/85, shuffle=True)
+    rf = RandomForestRegressor(n_estimators=100, max_depth=20, max_features='sqrt')
+    rfc.fit(Xtrain, ((threshold_1 >= Ytrain) | (threshold_2 <= Ytrain)))
+    rf.fit(Xtrain, Ytrain)
+    Ycalib_pred = rf.predict(Xcalib)
+    Ytest_pred = rf.predict(Xtest)
+    for i, fdp_nominal in enumerate(fdp_nominals):
+        calib_scores_2clip = 1000*((threshold_1 >= Ycalib) | (threshold_2 <= Ycalib)) - rfc.predict_proba(Xcalib)[:, 1]  # Ycalib_cs > 0.5 <=> original Ycalib_cs < threshold
+        test_scores = -rfc.predict_proba(Xtest)[:, 1]
+        # print(test_scores[1:3])
+        BH_2clip = BH(calib_scores_2clip, test_scores, fdp_nominal)
+        # print(BH_2clip)
+        fdp, power, cost,powers = eval_n(Ytest, BH_2clip, threshold_1, threshold_2)
+        fdpn_bcs.append(fdp)
+        powern_bcs.append(power)
+        costn_bcs.append(cost)
+        powers_bcs.append(powers)
+
+        calib_scores_2clip = 1000 * ((threshold_1 >= Ycalib) | (threshold_2 <= Ycalib)) - Ycalib_pred
+        test_scores = -Ytest_pred
+        # print(test_scores[1:3])
+        BH_2clip = BH(calib_scores_2clip, test_scores, fdp_nominal)
+        # print(BH_2clip)
+        fdp, power, cost, powers = eval_n(Ytest, BH_2clip, threshold_1, threshold_2)
+        fdpn_cs.append(fdp)
+        powern_cs.append(power)
+        costn_cs.append(cost)
+        powers_cs.append(powers)
+
+
+all_res['fdpn_bcs'] = fdpn_bcs
+all_res['costn_bcs'] = costn_bcs
+all_res['powern_bcs'] = powern_bcs
+all_res['powers_bcs'] = powers_bcs
+
+all_res['fdpn_cs'] = fdpn_cs
+all_res['costn_cs'] = costn_cs
+all_res['powern_cs'] = powern_cs
+all_res['powers_cs'] = powers_cs
+
+
+''' two stage conformal selection'''
+fdpn_cs2union, pcern_cs2union, powern_cs2union= [], [], []
+with Timer() as timer:
+    Xtrain, Xcalib, Ytrain, Ycalib = train_test_split(Xtc, Ytc, train_size=50/85, shuffle=True)
+    rf = RandomForestRegressor(n_estimators=100, max_depth=20, max_features='sqrt')
+    rf.fit(Xtrain, Ytrain)
+    Ycalib_pred = rf.predict(Xcalib)
+    Ytest_pred = rf.predict(Xtest)
+    for i, fdp_nominal in enumerate(fdp_nominals):
+        calib_scores_2clip = 1000 * (threshold_1 >= Ycalib)  - Ycalib_pred
+        test_scores = -Ytest_pred
+        BH_2clipstep1 = BH(calib_scores_2clip, test_scores, fdp_nominal)
+
+        calib_scores_2clip = 1000 * (threshold_2 <= Ycalib) - Ycalib_pred
+        test_scores = -Ytest_pred
+        BH_2clipstep2 = BH(calib_scores_2clip, test_scores, fdp_nominal)
+        BH_2clip_union = np.intersect1d(BH_2clipstep1, BH_2clipstep2)
+        BH_2clip = BH_2clip_union.astype(int)
+
+        fdp, pcer, power = eval(Ytest, BH_2clip, threshold_1, threshold_2)
+        fdpn_cs2union.append(fdp)
+        pcern_cs2union.append(pcer)
+        powern_cs2union.append(power)
+
+all_res['fdpn_cs2union'] = fdpn_cs2union
+all_res['pcern_cs2union'] = pcern_cs2union
+all_res['powern_cs2union'] = powern_cs2union
+
+
+
+out_dir = os.path.join('result-union', f'{dataset_name} {args.sample:.2f}')
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+all_res.to_csv(os.path.join('result-union', f'{dataset_name} {args.sample:.2f}', f'{dataset_name} {args.sample:.2f} {args.seed}.csv'))
---
+++ plot-settingI.py
@@ -0,0 +1,395 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+import seaborn as sns
+import argparse
+
+# Set ggplot style for the plots
+parser = argparse.ArgumentParser()
+# parser.add_argument('sample', type=float)
+parser.add_argument('seed', type=int)
+args = parser.parse_args()
+
+sample = 1.0
+n_itr = args.seed
+plt.style.use('ggplot')
+
+df_list = []
+dataset_list = ['3A4', 'CB1', 'DPP4', 'HIVINT', 'HIVPROT', 'LOGD', 'METAB', 'NK1', 'OX1', 'OX2', 'PGP', 'PPB', 'RAT_F',
+                'TDI', 'THROMBIN']
+
+for name in dataset_list:
+    df_ones = []
+    for j in range(1, 1 + n_itr):
+        df = pd.read_csv(
+            os.path.join("result-inter", f"{name} {sample:.2f}", f"{name} {sample:.2f} {j}.csv"))
+        df_ones.append(df)
+    df = pd.concat(df_ones).groupby("fdp_nominals", as_index=False).mean()
+    df_list.append(df)
+
+out_dir = os.path.join('figure-settingI')
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+'''comparison'''
+# four method
+# Create a grid for subplots
+fig, axs = plt.subplots(nrows=3, ncols=5, figsize=(18, 12))
+axs = axs.flatten()
+
+# Loop through datasets and plot the data on each subplot
+for i, name in enumerate(dataset_list):
+    ax = axs[i]
+
+    if i == 0:
+        # Plot data for each model and conformal method
+        line1, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs2inter'],
+                         label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+
+        # line2, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs2inter'],
+        #                  label='Two-stage Conformal Inter: FDP', marker='o', color='red', alpha=0.8)
+
+        line2, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs'],
+                         label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+
+        line3, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_sh'],
+                         label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+        ax.legend(loc='best', bbox_to_anchor=(4.9, -3.1), frameon=True, shadow=False, ncol=3, fontsize=22)
+    else:
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs2inter'],
+                label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+
+        # ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs2inter'],
+        #         label='Two-stage Conformal Inter: FDP', marker='o', color='red', alpha=0.8)
+
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs'],
+                label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_sh'],
+                label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+    # Set axis labels
+    ax.plot([0.05, 0.55], [0.05, 0.55], color='grey', alpha=0.7, linestyle='-.')
+    ax.set_title(f'{name}', fontsize=22)
+
+    # Add grid lines
+    ax.grid(True)
+
+# Adjust spacing between subplots
+fig.subplots_adjust(wspace=0.2, hspace=0.3, top=0.92, bottom=0.2, left=0.07, right=0.96)
+
+# Add global x and y labels, move them slightly outward
+fig.text(0.5, 0.13, 'Nominal FDP', ha='center', fontsize=22)  # Moved down slightly
+fig.text(0.03, 0.5, 'Observed FDP', va='center', rotation='vertical', fontsize=22)  # Moved left slightly
+
+# Title for the entire plot
+# fig.suptitle("Comparison: FDP control for all 15 Datasets", fontsize=16)
+# Display the plot
+plt.savefig(os.path.join("figure-settingI", "compfdpI.png"))
+# plt.show()
+
+####power
+# four method
+# Create a grid for subplots
+fig, axs = plt.subplots(nrows=3, ncols=5, figsize=(18, 12))
+axs = axs.flatten()
+# Loop through datasets and plot the data on each subplot
+for i, name in enumerate(dataset_list):
+    ax = axs[i]
+
+    if i == 0:
+        # Plot data for each model and conformal method
+        line1, = ax.plot(df_list[i]['fdpn_cs2union'], df_list[i]['powern_cs2inter'],
+                         label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+
+        # line2, = ax.plot(df_list[i]['fdpn_cs2inter'], df_list[i]['powern_cs2inter'],
+        #                  label='Two-stage Conformal Inter: Power', marker='o', color='red', alpha=0.8)
+
+        line2, = ax.plot(df_list[i]['fdpn_cs'], df_list[i]['powern_cs'],
+                         label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+
+        line3, = ax.plot(df_list[i]['fdpn_sh'], df_list[i]['powern_sh'],
+                         label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+        ax.legend(loc='best', bbox_to_anchor=(4.9, -3.1), frameon=True, shadow=False, ncol=3, fontsize=22)
+    else:
+        ax.plot(df_list[i]['fdpn_cs2union'], df_list[i]['powern_cs2inter'],
+                label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+        # ax.plot(df_list[i]['fdpn_cs2inter'], df_list[i]['powern_cs2inter'],
+        #         label='Two-stage Conformal Inter: Power', marker='o', color='red', alpha=0.8)
+        ax.plot(df_list[i]['fdpn_cs'], df_list[i]['powern_cs'],
+                label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+        ax.plot(df_list[i]['fdpn_sh'], df_list[i]['powern_sh'],
+                label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+    # Set axis labels
+    ax.set_title(f'{name}', fontsize=22)
+
+    # Add grid lines
+    ax.grid(True)
+
+# Adjust spacing between subplots
+fig.subplots_adjust(wspace=0.2, hspace=0.3, top=0.92, bottom=0.2, left=0.07, right=0.96)
+
+# Add global x and y labels, move them slightly outward
+fig.text(0.5, 0.13, 'Observed FDP', ha='center', fontsize=22)  # Moved down slightly
+fig.text(0.03, 0.5, 'Observed Power', va='center', rotation='vertical', fontsize=22)  # Moved left slightly
+
+# Title for the entire plot
+# fig.suptitle("Comparison: Power for all 15 Datasets", fontsize=16)
+
+# Display the plot
+plt.savefig(os.path.join("figure-settingI", "comppowerobI.png"))
+# plt.show()
+
+
+### ob fdp + power 4
+# Create a grid for subplots
+fig, axs = plt.subplots(nrows=3, ncols=5, figsize=(18, 12))
+axs = axs.flatten()
+# Loop through datasets and plot the data on each subplot
+for i, name in enumerate(dataset_list):
+    ax = axs[i]
+
+    if i == 0:
+        # Plot data for each model and conformal method
+        line1, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs2inter'],
+                         label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+
+        # line2, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs2inter'],
+        #                  label='Two-stage Conformal Inter: Power', marker='o', color='red', alpha=0.8)
+
+        line2, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs'],
+                         label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+
+        line3, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_sh'],
+                         label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+        ax.legend(loc='best', bbox_to_anchor=(4.9, -3.1), frameon=True, shadow=False, ncol=3, fontsize=22)
+    else:
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs2inter'],
+                label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+        # ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs2inter'],
+        #         label='Two-stage Conformal Inter: Power', marker='o', color='red', alpha=0.8)
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs'],
+                label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_sh'],
+                label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+    # Set axis labels
+    ax.set_title(f'{name}', fontsize=22)
+
+    # Add grid lines
+    ax.grid(True)
+
+# Adjust spacing between subplots
+fig.subplots_adjust(wspace=0.2, hspace=0.3, top=0.92, bottom=0.2, left=0.07, right=0.96)
+
+# Add global x and y labels, move them slightly outward
+fig.text(0.5, 0.13, 'Nominal FDP', ha='center', fontsize=22)  # Moved down slightly
+fig.text(0.03, 0.5, 'Observed Power', va='center', rotation='vertical', fontsize=22)  # Moved left slightly
+
+# Title for the entire plot
+# fig.suptitle("Comparison: Power for all 15 Datasets", fontsize=16)
+
+# Display the plot
+plt.savefig(os.path.join("figure-settingI", "comppowerI.png"))
+# plt.show()
+
+
+#######################0.1dataset########################
+# Set ggplot style for the plots
+parser = argparse.ArgumentParser()
+# parser.add_argument('sample', type=float)
+parser.add_argument('seed', type=int)
+args = parser.parse_args()
+
+sample = 0.10
+n_itr = args.seed
+plt.style.use('ggplot')
+
+df_list = []
+dataset_list = ['3A4', 'CB1', 'DPP4', 'HIVINT', 'HIVPROT', 'LOGD', 'METAB', 'NK1', 'OX1', 'OX2', 'PGP', 'PPB', 'RAT_F',
+                'TDI', 'THROMBIN']
+
+for name in dataset_list:
+    df_ones = []
+    for j in range(1, 1 + n_itr):
+        df = pd.read_csv(
+            os.path.join("result-inter", f"{name} {sample:.2f}", f"{name} {sample:.2f} {j}.csv"))
+        df_ones.append(df)
+    df = pd.concat(df_ones).groupby("fdp_nominals", as_index=False).mean()
+    df_list.append(df)
+
+out_dir = os.path.join('figure-settingI')
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+'''comparison'''
+
+# four method
+# Create a grid for subplots
+fig, axs = plt.subplots(nrows=3, ncols=5, figsize=(18, 12))
+axs = axs.flatten()
+
+# Loop through datasets and plot the data on each subplot
+for i, name in enumerate(dataset_list):
+    ax = axs[i]
+
+    if i == 0:
+        # Plot data for each model and conformal method
+        line1, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs2inter'],
+                         label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+
+        # line2, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs2inter'],
+        #                  label='Two-stage Conformal Inter: FDP', marker='o', color='red', alpha=0.8)
+
+        line2, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs'],
+                         label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+
+        line3, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_sh'],
+                         label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+        ax.legend(loc='best', bbox_to_anchor=(4.9, -3.1), frameon=True, shadow=False, ncol=3, fontsize=22)
+    else:
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs2inter'],
+                label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+
+        # ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs2inter'],
+        #         label='Two-stage Conformal Inter: FDP', marker='o', color='red', alpha=0.8)
+
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_cs'],
+                label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['fdpn_sh'],
+                label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+    # Set axis labels
+    ax.plot([0.05, 0.55], [0.05, 0.55], color='grey', alpha=0.7, linestyle='-.')
+    ax.set_title(f'{name}', fontsize=22)
+
+    # Add grid lines
+    ax.grid(True)
+
+# Adjust spacing between subplots
+fig.subplots_adjust(wspace=0.2, hspace=0.3, top=0.92, bottom=0.2, left=0.07, right=0.96)
+
+# Add global x and y labels, move them slightly outward
+fig.text(0.5, 0.13, 'Nominal FDP', ha='center', fontsize=22)  # Moved down slightly
+fig.text(0.03, 0.5, 'Observed FDP', va='center', rotation='vertical', fontsize=22)  # Moved left slightly
+
+# Title for the entire plot
+# fig.suptitle("Comparison: FDP control for all 15 Datasets", fontsize=16)
+# Display the plot
+plt.savefig(os.path.join("figure-settingI", "compfdpI0.1.png"))
+# plt.show()
+
+
+
+####power
+# four method
+# Create a grid for subplots
+fig, axs = plt.subplots(nrows=3, ncols=5, figsize=(18, 12))
+axs = axs.flatten()
+# Loop through datasets and plot the data on each subplot
+for i, name in enumerate(dataset_list):
+    ax = axs[i]
+
+    if i == 0:
+        # Plot data for each model and conformal method
+        line1, = ax.plot(df_list[i]['fdpn_cs2union'], df_list[i]['powern_cs2inter'],
+                         label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+
+        # line2, = ax.plot(df_list[i]['fdpn_cs2inter'], df_list[i]['powern_cs2inter'],
+        #                  label='Two-stage Conformal Inter: Power', marker='o', color='red', alpha=0.8)
+
+        line2, = ax.plot(df_list[i]['fdpn_cs'], df_list[i]['powern_cs'],
+                         label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+
+        line3, = ax.plot(df_list[i]['fdpn_sh'], df_list[i]['powern_sh'],
+                         label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+        ax.legend(loc='best', bbox_to_anchor=(4.9, -3.1), frameon=True, shadow=False, ncol=3, fontsize=22)
+    else:
+        ax.plot(df_list[i]['fdpn_cs2union'], df_list[i]['powern_cs2inter'],
+                label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+        # ax.plot(df_list[i]['fdpn_cs2inter'], df_list[i]['powern_cs2inter'],
+        #         label='Two-stage Conformal Inter: Power', marker='o', color='red', alpha=0.8)
+        ax.plot(df_list[i]['fdpn_cs'], df_list[i]['powern_cs'],
+                label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+        ax.plot(df_list[i]['fdpn_sh'], df_list[i]['powern_sh'],
+                label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+    # Set axis labels
+    ax.set_title(f'{name}', fontsize=22)
+
+    # Add grid lines
+    ax.grid(True)
+
+# Adjust spacing between subplots
+fig.subplots_adjust(wspace=0.2, hspace=0.3, top=0.92, bottom=0.2, left=0.07, right=0.96)
+
+# Add global x and y labels, move them slightly outward
+fig.text(0.5, 0.13, 'Observed FDP', ha='center', fontsize=22)  # Moved down slightly
+fig.text(0.03, 0.5, 'Observed Power', va='center', rotation='vertical', fontsize=22)  # Moved left slightly
+
+# Title for the entire plot
+# fig.suptitle("Comparison: Power for all 15 Datasets", fontsize=16)
+
+# Display the plot
+plt.savefig(os.path.join("figure-settingI", "comppowerobI0.1.png"))
+# plt.show()
+
+### ob fdp + power 4
+# Create a grid for subplots
+fig, axs = plt.subplots(nrows=3, ncols=5, figsize=(18, 12))
+axs = axs.flatten()
+# Loop through datasets and plot the data on each subplot
+for i, name in enumerate(dataset_list):
+    ax = axs[i]
+
+    if i == 0:
+        # Plot data for each model and conformal method
+        line1, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs2inter'],
+                         label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+
+        line2, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs'],
+                         label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+
+        line3, = ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_sh'],
+                         label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+        ax.legend(loc='best', bbox_to_anchor=(4.9, -3.1), frameon=True, shadow=False, ncol=3, fontsize=22)
+    else:
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs2inter'],
+                label='Baseline Conformal', marker='o', color='red', alpha=0.8)
+        # ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs2inter'],
+        #         label='Two-stage Conformal Inter: Power', marker='o', color='red', alpha=0.8)
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_cs'],
+                label='RSI Conformal', marker='o', color='darkgreen', alpha=0.8)
+        ax.plot(df_list[i]['fdp_nominals'], df_list[i]['powern_sh'],
+                label='RSI Sheridan', marker='o', color='steelblue', alpha=0.8)
+
+    # Set axis labels
+    ax.set_title(f'{name}', fontsize=22)
+
+    # Add grid lines
+    ax.grid(True)
+
+# Adjust spacing between subplots
+fig.subplots_adjust(wspace=0.2, hspace=0.3, top=0.92, bottom=0.2, left=0.07, right=0.96)
+
+# Add global x and y labels, move them slightly outward
+fig.text(0.5, 0.13, 'Nominal FDP', ha='center', fontsize=22)  # Moved down slightly
+fig.text(0.03, 0.5, 'Observed Power', va='center', rotation='vertical', fontsize=22)  # Moved left slightly
+
+# Title for the entire plot
+# fig.suptitle("Comparison: Power for all 15 Datasets", fontsize=16)
+
+# Display the plot
+plt.savefig(os.path.join("figure-settingI", "comppowerI0.1.png"))
+# plt.show()
